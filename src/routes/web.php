17bd2d7b8ca7b012fdbac7d649d70bf46e616a0b
koharu <le2.lsb108@gmail.com>
2024-11-15 22:42:24 +0900

    Fortifyインストール・初期設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Fortify認証機能。Controllerの設定要
+// Route::middleware('auth')->group(function () {
+//     Route::get('/', [AuthController::class, 'index']);
+// });
