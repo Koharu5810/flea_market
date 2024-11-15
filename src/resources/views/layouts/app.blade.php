d675b015c5c270e6c062dd52414c8796d048f50b
koharu <le2.lsb108@gmail.com>
2024-11-16 00:41:12 +0900

    appブレードファイル修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -13,7 +13,32 @@
 <body>
     <header>
         <div class="header">
+            {{-- ロゴ表示 --}}
             <img class="header__logo" src="{{ asset('storage/images/logo.svg') }}" alt="ロゴ">
+
+            {{-- 会員登録画面・ログイン画面: ロゴのみ --}}
+            @if (request()->is('register') || request()->is('login'))
+            {{-- その他画面 --}}
+            @else
+                <div class="header__menu">
+                    {{-- 検索フォーム --}}
+                    <form action="{{ route('search') }}" method="GET" class="header__search">
+                        <input type="text" name="query" placeholder="なにをお探しですか？" class="header__search-input">
+                    </form>
+
+                    {{-- ログアウトボタン --}}
+                    <form action="{{ route('logout') }}" method="POST" class="header__logout">
+                        @csrf
+                        <button type="submit" class="header__logout-button">ログアウト</button>
+                    </form>
+
+                    {{-- マイページボタン --}}
+                    <a href="{{ route('profile.index') }}" class="header__mypage">マイページ</a>
+
+                    {{-- 出品ボタン --}}
+                    <a href="{{ route('sell') }}" class="header__sell-button">出品する</a>
+                </div>
+            @endif
         </div>
     </header>
 
