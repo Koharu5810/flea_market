13dfc014d7b010209ec0937fd710d7becd4dbde0
koharu <le2.lsb108@gmail.com>
2024-11-17 16:03:51 +0900

    ProfileController作成

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\UserAddress;
+use App\Http\Requests\AddressRequest;
+use App\Http\Requests\ProfileRequest;
 
 class UserController extends Controller
 {
     public function index()
     {
         return view('profile.index');
+    }
+
+    public function store(Request $request)
+    {
+        $profileData = app(ProfileRequest::class)->validated();
+        $addressData = app(AddressRequest::class)->validated();
+
+        $user = User::create([
+            'username' => $profileData['username'],
+        ]);
+
+        UserAddress::create([
+            'user_id' => $user->id,
+            'postal_code' => $addressData['postal_code'],
+            'address' => $addressData['address'],
+            'building' => $addressData['building'],
+        ]);
+
+        return redirect()->route('home');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -37,5 +37,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/', [ItemController::class, 'index'])->name('home');
     // プロフィール画面
     Route::get('/mypage', [UserController::class, 'index'])->name('profile.index');
+    Route::post('/mypage', [UserController::class, 'store']);
 });
 
