9a45306a63e90c806557aef8c6e55edcb8bd1e91
koharu <le2.lsb108@gmail.com>
2024-11-30 12:43:44 +0900

    商品購入機能（stripe）実装準備

--- src/resources/views/purchase/index.blade.php
+++ src/resources/views/purchase/index.blade.php
@@ -32,7 +32,7 @@
                 <h3 class="parts-title">配送先</h3>
                 <form method="GET" action="{{ route('show.purchase.address', ['item_id' => $item->id]) }}">
                     @csrf
-                    <a type="submit" class="change-address-button blue-button">変更する</a>
+                    <button type="submit" class="change-address-button blue-button">変更する</button>
                 </form>
             </div>
             <div class="delivery-detail">
@@ -43,18 +43,18 @@
         </div>
     {{-- 右側 --}}
         <div class="right-container">
-            <table class="confirmation-table">
-                <tr>
-                    <th>商品代金</th>
-                    <td>&yen;<span>{{ number_format($item->price) }}</span></td>
-                </tr>
-                <tr>
-                    <th>支払い方法</th>
-                    <td id="payment-method">選択してください</td>
-                </tr>
-            </table>
             {{-- <form method="post" action=""> --}}
                 {{-- @csrf --}}
+                <table class="confirmation-table">
+                    <tr>
+                        <th>商品代金</th>
+                        <td>&yen;<span>{{ number_format($item->price) }}</span></td>
+                    </tr>
+                    <tr>
+                        <th>支払い方法</th>
+                        <td id="payment-method">選択してください</td>
+                    </tr>
+                </table>
                 <button type="submit" class="purchase-button red-button">購入する</button>
             {{-- </form> --}}
         </div>
