f072cbf4b5a1478deb829d45699179bd4b675b2e
koharu <le2.lsb108@gmail.com>
2024-12-07 15:42:19 +0900

    registerビュー／レスポンシブにCSS調整

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -8,54 +8,51 @@
 @section('title', '会員登録')
 
 @section('content')
-    <div class="register-container">
-        <form method="post" action="/register">
-        @csrf
-    {{-- ユーザー名 --}}
-            <div class="register-form__group">
-                <label for="username">ユーザー名</label>
-                <input type="text" name="username" value="{{ old('username') }}" class="register-form__group-input" />
-                <div class="error-message">
-                    @error('username')
-                        {{ $message }}
-                    @enderror
-                </div>
+    <form method="post" action="/register" class="register-container">
+    @csrf
+{{-- ユーザー名 --}}
+        <div class="register-form__group">
+            <label for="username">ユーザー名</label>
+            <input type="text" name="username" value="{{ old('username') }}" class="register-form__group-input" />
+            <div class="error-message">
+                @error('username')
+                    {{ $message }}
+                @enderror
             </div>
-    {{-- メールアドレス --}}
-            <div class="register-form__group">
-                <label for="username">メールアドレス</label>
-                <input type="email" name="email" value="{{ old('email') }}" class="register-form__group-input" />
-                <div class="error-message">
-                    @error('email')
-                        {{ $message }}
-                    @enderror
-                </div>
+        </div>
+{{-- メールアドレス --}}
+        <div class="register-form__group">
+            <label for="username">メールアドレス</label>
+            <input type="email" name="email" value="{{ old('email') }}" class="register-form__group-input" />
+            <div class="error-message">
+                @error('email')
+                    {{ $message }}
+                @enderror
             </div>
-    {{-- パスワード --}}
-            <div class="register-form__group">
-                <label for="password">パスワード</label>
-                <input type="password" name="password"  class="register-form__group-input" />
-                <div class="error-message">
-                    @error('password')
-                        {{ $message }}
-                    @enderror
-                </div>
+        </div>
+{{-- パスワード --}}
+        <div class="register-form__group">
+            <label for="password">パスワード</label>
+            <input type="password" name="password"  class="register-form__group-input" />
+            <div class="error-message">
+                @error('password')
+                    {{ $message }}
+                @enderror
             </div>
-    {{-- 確認用パスワード --}}
-            <div class="register-form__group">
-                <label for="password_confirmation">確認用パスワード</label>
-                <input type="password" name="password_confirmation" class="register-form__group-input" />
-                <div class="error-message">
-                    @error('password_confirmation')
-                        {{ $message }}
-                    @enderror
-                </div>
+        </div>
+{{-- 確認用パスワード --}}
+        <div class="register-form__group">
+            <label for="password_confirmation">確認用パスワード</label>
+            <input type="password" name="password_confirmation" class="register-form__group-input" />
+            <div class="error-message">
+                @error('password_confirmation')
+                    {{ $message }}
+                @enderror
             </div>
-    {{-- 登録ボタン --}}
-            <button class="register-form__button form__red-button">登録する</button>
-        </form>
+        </div>
+{{-- 登録ボタン --}}
+        <button class="register-form__button form__red-button">登録する</button>
+    </form>
 {{-- ログイン案内 --}}
-        <a href="/login" class="login-button blue-button">ログインはこちら</a>
-
-    </div>
+    <a href="/login" class="login-button blue-button">ログインはこちら</a>
 @endsection
