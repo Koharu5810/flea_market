f3b5fb78a762be3689988d5bfa21b54347d56820
koharu <le2.lsb108@gmail.com>
2024-12-07 16:53:10 +0900

    profile_editビュー／レスポンシブにCSS調整

--- src/resources/views/profile/profile_edit.blade.php
+++ src/resources/views/profile/profile_edit.blade.php
@@ -8,99 +8,95 @@
 @section('title', 'プロフィール設定')
 
 @section('content')
-    <div class="profile__edit-container">
-        <form method="post" action="{{ route('profile.save') }}" enctype="multipart/form-data">
-        @csrf
-    {{-- プロフィール画像 --}}
-            <div class="profile__image">
-                <div class="profile__image-preview" id="imagePreview">
-                    <img id="previewImage"
-                        src="{{ isset($profileImage) ? $profileImage : '' }}"
-                        alt="プロフィールアイコン"
-                        style="{{ isset($profileImage) ? '' : 'display: none;' }}" />
-                </div>
-                <label class="edit__red-button">
-                    画像を選択する
-                    <input
-                        type="file"
-                        name="profile_image"
-                        id="imageInput"
-                        accept="image/*"
-                        style="display: none;"
-                    />
-                </label>
+    <form method="post" action="{{ route('profile.save') }}" enctype="multipart/form-data" class="profile__edit-container">
+    @csrf
+{{-- プロフィール画像 --}}
+        <div class="profile__image">
+            <div class="profile__image-preview" id="imagePreview">
+                <img id="previewImage"
+                    src="{{ isset($profileImage) ? $profileImage : '' }}"
+                    alt="プロフィールアイコン"
+                    style="{{ isset($profileImage) ? '' : 'display: none;' }}" />
             </div>
-            @error('profile_image')
+            <label class="edit__red-button">
+                画像を選択する
+                <input
+                    type="file"
+                    name="profile_image"
+                    id="imageInput"
+                    accept="image/*"
+                    style="display: none;"
+                />
+            </label>
+        </div>
+        @error('profile_image')
+            <div class="error-message">
+                {{ $message }}
+            </div>
+        @enderror
+{{-- ユーザー名 --}}
+        <div class="form__group">
+            <label for="username">ユーザー名</label>
+            <input
+                type="text"
+                name="username"
+                value="{{ old('username', $username) }}"
+                class="form__group-input"
+            />
+            @error('username')
                 <div class="error-message">
                     {{ $message }}
                 </div>
             @enderror
-    {{-- ユーザー名 --}}
-            <div class="form__group">
-                <label for="username">ユーザー名</label>
-                <input
-                    type="text"
-                    name="username"
-                    value="{{ old('username', $username) }}"
-                    class="form__group-input"
-                />
-                @error('username')
-                    <div class="error-message">
+        </div>
+{{-- 郵便番号 --}}
+        <div class="form__group">
+            <label for="postal_code">郵便番号</label>
+            <input
+                type="text"
+                name="postal_code"
+                value="{{ old('postal_code', $userAddress->postal_code) }}"
+                class="form__group-input"
+            />
+            @error('postal_code')
+                <div class="error-message">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+{{-- 住所 --}}
+        <div class="form__group">
+            <label for="address">住所</label>
+            <input
+                type="text"
+                name="address"
+                value="{{ old('address', $userAddress->address) }}"
+                class="form__group-input"
+            />
+            @error('address')
+                <div class="error-message">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+{{-- 建物名 --}}
+        <div class="form__group">
+            <label for="building">建物名</label>
+            <input
+                type="text"
+                name="building"
+                value="{{ old('building', $userAddress->building) }}"
+                class="form__group-input"
+            />
+            @error('building')
+                <div class="error-message">
                         {{ $message }}
-                    </div>
-                @enderror
-            </div>
-    {{-- 郵便番号 --}}
-            <div class="form__group">
-                <label for="postal_code">郵便番号</label>
-                <input
-                    type="text"
-                    name="postal_code"
-                    value="{{ old('postal_code', $userAddress->postal_code) }}"
-                    class="form__group-input"
-                />
-                @error('postal_code')
-                    <div class="error-message">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-    {{-- 住所 --}}
-            <div class="form__group">
-                <label for="address">住所</label>
-                <input
-                    type="text"
-                    name="address"
-                    value="{{ old('address', $userAddress->address) }}"
-                    class="form__group-input"
-                />
-                @error('address')
-                    <div class="error-message">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-    {{-- 建物名 --}}
-            <div class="form__group">
-                <label for="building">建物名</label>
-                <input
-                    type="text"
-                    name="building"
-                    value="{{ old('building', $userAddress->building) }}"
-                    class="form__group-input"
-                />
-                @error('building')
-                    <div class="error-message">
-                            {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            {{-- 更新ボタン --}}
-            <div class="profile-form__button">
-                <button class="form__red-button">更新する</button>
-            </div>
-        </form>
-    </div>
+                </div>
+            @enderror
+        </div>
+        {{-- 更新ボタン --}}
+        <button class="profile-form__button form__red-button">更新する</button>
+    </form>
 
 <script>
     const imageInput = document.getElementById('imageInput');
