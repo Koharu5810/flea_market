e8a52e2b4c12b470f5ef65deed4f6040daf80df9
koharu <le2.lsb108@gmail.com>
2024-12-07 21:18:33 +0900

    purchase.addressビュー／レスポンシブにCSS調整

--- src/resources/views/purchase/address.blade.php
+++ src/resources/views/purchase/address.blade.php
@@ -8,19 +8,18 @@
 @section('title', '住所の変更')
 
 @section('content')
-    <div class="profile__create-container">
-        <form method="POST" action="{{ route('update.purchase.address', ['item_id' => $item->id]) }}">
+        <form method="POST" action="{{ route('update.purchase.address', ['item_id' => $item->id]) }}" class="address__update-container">
             @csrf
             @method('PATCH')
     {{-- 郵便番号 --}}
-            <div class="profile-form__group">
+            <div class="form__group">
                 <label for="postal_code">郵便番号</label>
                 <input
                     type="text"
                     id="postal_code"
                     name="postal_code"
                     value="{{ old('postal_code', $address->postal_code) }}"
-                    class="profile-form__group-input"
+                    class="form__group-input"
                 />
                 @error('postal_code')
                     <p class="error-message">
@@ -29,14 +28,14 @@
                 @enderror
             </div>
     {{-- 住所 --}}
-            <div class="profile-form__group">
+            <div class="form__group">
                 <label for="address">住所</label>
                 <input
                     type="text"
                     id="address"
                     name="address"
                     value="{{ old('address', $address->address) }}"
-                    class="profile-form__group-input"
+                    class="form__group-input"
                 />
                 @error('address')
                     <p class="error-message">
@@ -45,14 +44,14 @@
                 @enderror
             </div>
     {{-- 建物名 --}}
-            <div class="profile-form__group">
+            <div class="form__group">
                 <label for="building">建物名</label>
                 <input
                     type="text"
                     id="building"
                     name="building"
                     value="{{ old('building', $address->building) }}"
-                    class="profile-form__group-input"
+                    class="form__group-input"
                 />
                 @error('building')
                     <p class="error-message">
@@ -63,6 +62,5 @@
     {{-- 登録ボタン --}}
             <button type="submit" class="profile-form__button form__red-button">更新する</button>
         </form>
-    </div>
 
 @endsection
