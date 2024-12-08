33a705f76b40e97bae2297e327c1f025647b5924
koharu <le2.lsb108@gmail.com>
2024-12-08 15:41:33 +0900

    LoginTest／メールアドレス（ユーザ名）が空の場合のテスト作成

--- src/tests/Feature/LoginTest.php
+++ src/tests/Feature/LoginTest.php
@@ -13,10 +13,30 @@ class LoginTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // CSRF トークン検証を無効化
+        $this->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+    }
+
+    public function test_email_validation_error_when_email_is_missing()
+    {
+        $url = route('login');
+
+        $data = [
+            'username' => '',
+            'password' => 'password123',
+            'password_confirmation' => 'password123',
+        ];
+
+        $response = $this->postJson($url, $data);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors([
+            'username' => 'ユーザー名またはメールアドレスを入力してください',
+        ]);
     }
 }
