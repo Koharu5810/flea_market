b93e91b6e5fa8688fcbcb914bf9b1906e6c30144
koharu <le2.lsb108@gmail.com>
2024-12-14 14:03:40 +0900

    ItemTest／購入済み商品表示テスト作成

--- src/tests/Feature/ItemTest.php
+++ src/tests/Feature/ItemTest.php
@@ -4,7 +4,10 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Tests\TestCase;
+use App\Models\Item;
 
 class ItemTest extends TestCase
 {
@@ -13,10 +16,27 @@ class ItemTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    public function test_show_all_items()
+    {
+        $response = $this->get(route('home'));
         $response->assertStatus(200);
+
+        Item::all();
+    }
+    public function test_purchased_items_are_marked_as_sold()
+    {
+        $this->seed();
+        $items = Item::all();
+
+        $soldItems = $items->random(3);
+        foreach ($soldItems as $item) {
+            $item->update(['is_sold' => true]);
+        }
+
+        $response = $this->get(route('home'));
+        $response->assertStatus(200);
+        // $response->assertSee('Sold');
     }
 }
