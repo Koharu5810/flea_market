f0fa857459dd197a67a397f47c5e4848a10022df
koharu <le2.lsb108@gmail.com>
2024-12-15 15:57:56 +0900

    FavoriteTest1／処理分離

--- src/tests/Feature/FavoriteTest.php
+++ src/tests/Feature/FavoriteTest.php
@@ -19,7 +19,7 @@ class FavoriteTest extends TestCase
      */
     use RefreshDatabase;
 
-    public function test_item_can_be_favorited_by_clicking_favorite_icon()
+    public function openItemDetailPage()
     {
         $user = TestHelper::userLogin();
         $this->seed();
@@ -28,7 +28,24 @@ class FavoriteTest extends TestCase
         $response = $this->get(route('item.detail', ['item_id' => $item->id]));
         $response->assertStatus(200);
 
-        $favoriteIcon = $item->isFavoriteBy(auth()->user())->asset('storage/app/favorited.png');
-        $response->assertSee($favoriteIcon);
+        return [$response, $item, $user];
+    }
+
+    public function test_item_can_be_favorited_by_clicking_favorite_icon()
+    {
+        [$response, $item, $user] = $this->openItemDetailPage();
+
+        $this->post(route('item.favorite', ['item_id' => $item->id]))
+            ->assertStatus(302)
+            ->assertRedirect(route('item.detail', ['item_id' => $item->id]));
+
+        $this->assertDatabaseHas('favorites', [
+            'user_id' => $user->id,
+            'item_id' => $item->id,
+        ]);
+
+        $favoriteIcon = asset('storage/app/favorited.png');
+        $response = $this->get(route('item.detail', ['item_id' => $item->id]));
+        $response->assertSee($favoriteIcon, false);
     }
 }
