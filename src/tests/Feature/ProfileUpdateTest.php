7d7ced92d86168468578511072d3bc68fc2c3c49
koharu <le2.lsb108@gmail.com>
2024-12-21 23:55:05 +0900

    ProfileUpdateTest作成

--- src/tests/Feature/ProfileUpdateTest.php
+++ src/tests/Feature/ProfileUpdateTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\UserAddress;
 use Tests\Helpers\TestHelper;
 
 class ProfileUpdateTest extends TestCase
@@ -20,7 +21,21 @@ class ProfileUpdateTest extends TestCase
     {
         $user = TestHelper::userLogin();
 
+        $user->update([
+            'profile_image' => 'images/profile/test_image.png',
+            'username' => 'TestUser',
+            'postal_code' => '123-4567',
+            'address' => 'テスト住所',
+            'building' => 'テストビル',
+        ]);
+
         $response = $this->get(route('profile.edit', $user->id));
         $response->assertStatus(200);
+
+        $response->assertSee(asset('storage/' . $user->profile_image));
+        $response->assertSee($user->username);
+        $response->assertSee($user->postal_code);
+        $response->assertSee($user->address);
+        $response->assertSee($user->building);
     }
 }
