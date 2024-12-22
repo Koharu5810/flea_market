aaa106ae26af7f406cc5f9904728f9e29eb3dafb
koharu <le2.lsb108@gmail.com>
2024-12-22 22:08:59 +0900

    ItemPurchaseTest／購入処理テスト作成

--- src/tests/Feature/ItemPurchaseTest.php
+++ src/tests/Feature/ItemPurchaseTest.php
@@ -21,6 +21,11 @@ class ItemPurchaseTest extends TestCase
     private function purchasePageShow()
     {
         $user = TestHelper::userLogin();
+        $user->user_address()->create([
+            'postal_code' => '123-4567',
+            'address' => '東京都新宿区テスト',
+            'building' => 'テストビル',
+        ]);
 
         $categories = Category::take(2)->get();
         $item = Item::factory()->create(['name' => 'Test Item']);
@@ -30,11 +35,33 @@ class ItemPurchaseTest extends TestCase
         $response->assertStatus(200);
         $response->assertSee('購入する');
 
-        return [$user, $item];
+        return [$user, $item, $response];
     }
-    public function test_example()
+    public function test_user_can_purchase_item()
     {
-        $user = TestHelper::userLogin();
+        [$user, $item, $response] = $this->purchasePageShow();
 
+        // Stripe関連の処理をモック
+        $this->mock(\Stripe\Stripe::class, function ($mock) {
+        });
+        $this->mock(\Stripe\Checkout\Session::class, function ($mock) use ($item) {
+            $mock->shouldReceive('retrieve')
+                ->andReturn((object)[
+                    'payment_status' => 'paid',
+                    'payment_method_types' => ['card'],
+                ]);
+        });
+
+        // テーブルの確認
+        // $this->assertDatabaseHas('orders', [
+        //     'user_id' => $user->id,
+        //     'item_id' => $item->id,
+        //     'payment_method' => 'card',
+        // ]);
+
+        // $this->assertDatabaseHas('items', [
+        //     'id' => $item->id,
+        //     'is_sold' => true,
+        // ]);
     }
 }
