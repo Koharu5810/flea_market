07773072b43c276f3d520da3d259481d3dedecc1
koharu <le2.lsb108@gmail.com>
2024-12-22 17:51:49 +0900

    ItemPurchaseTest作成

--- src/tests/Feature/CommentTest.php
+++ src/tests/Feature/CommentTest.php
@@ -3,11 +3,9 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Item;
 use App\Models\Category;
-use Psy\CodeCleaner\ReturnTypePass;
 use Tests\Helpers\TestHelper;
 
 class CommentTest extends TestCase
--- src/tests/Feature/ItemPurchaseTest.php
+++ src/tests/Feature/ItemPurchaseTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Tests\Helpers\TestHelper;
+use App\Models\Item;
+use App\Models\Category;
 
 class ItemPurchaseTest extends TestCase
 {
@@ -16,11 +18,23 @@ class ItemPurchaseTest extends TestCase
      */
     use RefreshDatabase;
 
+    private function purchasePageShow()
+    {
+        $user = TestHelper::userLogin();
+
+        $categories = Category::take(2)->get();
+        $item = Item::factory()->create(['name' => 'Test Item']);
+        $item->categories()->attach($categories->pluck('id'));
+
+        $response = $this->get(route('purchase.show', ['item_id' => $item->id]));
+        $response->assertStatus(200);
+        $response->assertSee('購入する');
+
+        return [$user, $item];
+    }
     public function test_example()
     {
         $user = TestHelper::userLogin();
 
-        $response = $this->get('/');
-        $response->assertStatus(200);
     }
 }
