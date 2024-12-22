97e311881f372b119815fb7db4af6c08606853d6
koharu <le2.lsb108@gmail.com>
2024-12-22 22:54:29 +0900

    ItemPurchaseTest／購入処理テスト修正

--- src/tests/Feature/ItemPurchaseTest.php
+++ src/tests/Feature/ItemPurchaseTest.php
@@ -4,10 +4,12 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 use Tests\Helpers\TestHelper;
 use App\Models\Item;
 use App\Models\Category;
+use App\Models\UserAddress;
 
 class ItemPurchaseTest extends TestCase
 {
@@ -27,6 +29,8 @@ class ItemPurchaseTest extends TestCase
             'building' => 'テストビル',
         ]);
 
+        $address = UserAddress::factory()->create(['user_id' => $user->id]);
+
         $categories = Category::take(2)->get();
         $item = Item::factory()->create(['name' => 'Test Item']);
         $item->categories()->attach($categories->pluck('id'));
@@ -35,11 +39,11 @@ class ItemPurchaseTest extends TestCase
         $response->assertStatus(200);
         $response->assertSee('購入する');
 
-        return [$user, $item, $response];
+        return [$user, $address, $item, $response];
     }
     public function test_user_can_purchase_item()
     {
-        [$user, $item, $response] = $this->purchasePageShow();
+        [$user, $address, $item, $response] = $this->purchasePageShow();
 
         // Stripe関連の処理をモック
         $this->mock(\Stripe\Stripe::class, function ($mock) {
@@ -52,10 +56,25 @@ class ItemPurchaseTest extends TestCase
                 ]);
         });
 
+        $data = [
+            'uuid' => (string) Str::uuid(),
+            'user_id' => auth()->id(),
+            'item_id' => $item->id,
+            'address_id' => $address->id,
+            'payment_method' => $session->payment_method_types[0] ?? 'unknown',
+            'purchased_at' => now(),
+        ];
+
+        $response = $this->post(route('purchase.checkout', ['item_id' => $item->id]), $data);
+        // $response->assertRedirect(route('purchase.success', ['item_id' => $item->id]));
+
+        $response->assertStatus(302);
+
         // テーブルの確認
         // $this->assertDatabaseHas('orders', [
         //     'user_id' => $user->id,
         //     'item_id' => $item->id,
+        //     'address_id' => $address->id,
         //     'payment_method' => 'card',
         // ]);
 
@@ -63,5 +82,7 @@ class ItemPurchaseTest extends TestCase
         //     'id' => $item->id,
         //     'is_sold' => true,
         // ]);
+
+        // $response->assertRedirect(route('home'));
     }
 }
